1d316484d88e8a9f0fcdbc63595c9c3f6bbad63b
samuel Clavijo <saclavij@comcast.net>
2021-02-12 07:20:28 -0500

    adding buttons and constraint not rotating

---
+++ starter/app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -0,0 +1,29 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginFragment : Fragment() {
+
+
+    //Inflating and Returning the View with DataBindingUtil
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding: FragmentLoginBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_login, container, false)
+        return binding.root
+    }
+
+
+}
